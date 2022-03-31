66b70f81c65df34dc204bb2e06025e22c52c5803
ChrisKareh <64197212+ChrisKareh@users.noreply.github.com>
2022-03-31 16:04:34 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by k u r i s u on 3/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
